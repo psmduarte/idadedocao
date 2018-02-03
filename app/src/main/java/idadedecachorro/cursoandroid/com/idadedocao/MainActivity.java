84491e763a48101c476323f4491f5fd850c3219e
psmduarte <psmduarte@live.com>
2018-02-03 18:58:16 +0000

    Initial commit

---
+++ app/src/main/java/idadedecachorro/cursoandroid/com/idadedocao/MainActivity.java
@@ -0,0 +1,15 @@
+package idadedecachorro.cursoandroid.com.idadedocao;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
